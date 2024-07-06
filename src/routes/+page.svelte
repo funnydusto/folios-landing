c11c6a5b3d10081a9b1086340e8be362e82fd097
funnydusto <dustopersonal@gmail.com>
2024-07-06 06:21:18 -0400

    landing page complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,35 @@
 <script lang="ts">
     import MarketingNavbar from "$components/MarketingNavbar.svelte";
+    import Button from "$components/Button.svelte";
+    import Discord from "$lib/icons/discord.svg?raw";
+    import { slide } from "svelte/transition";
+    import { sineInOut } from "svelte/easing";
 
-    const genreArray = [
-        {
-            label: "moderator",
-            color: "text-lightblue-400",
-        },
-        {
-            label: "developer",
-            color: "text-orange-400",
-        },
-        {
-            label: "designer",
-            color: "text-purple-400",
-        },
-        {
-            label: "event host",
-            color: "text-yellow-400",
-        },
-        {
-            label: "manager",
-            color: "text-blue-400",
-        },
-        {
-            label: "researcher",
-            color: "text-green-400",
-        },
-        {
-            label: "creator",
-            color: "text-pink-400",
-        },
-    ]
+    const easing = sineInOut;
 
-    let genre = genreArray[1]
+
+    interface Genre {
+        label: string;
+        color: string;
+    }
+
+    const genreArray: Genre[] = [
+        { label: "moderator", color: "text-lightblue-500" },
+        { label: "developer", color: "text-orange-400" },
+        { label: "designer", color: "text-purple-400" },
+        { label: "event host", color: "text-yellow-500" },
+        { label: "manager", color: "text-blue-400" },
+        { label: "researcher", color: "text-green-600" },
+        { label: "creator", color: "text-pink-400" }, ]
+
+    let index = 0;
+
+    $: genre = genreArray[index];
+
+    setInterval(() => {
+        index = (index + 1) % genreArray.length;
+    }, 5000);
+
 
 </script>
 
@@ -40,10 +37,22 @@
 <MarketingNavbar/>
 
 <!-- Hero -->
-<div class="border-gray-200 border-2 rounded-xl px-6 py-24">
-    <h1 class="font-display text-4xl text-center">Find your next <span class="text-wrap {genre.color}">{genre.label}</span> on folios.cc</h1>
-    <p>Find & explore community builders for your Discord server.
-
-        Request features & get updates in our server below</p>
+<div class="flex flex-col items-center h-full gap-4 px-6 py-24 text-center md:text-left bg-[url('landing_bg_mobile.png')] bg-bottom md:bg-[url('landing_bg_desktop.png')] md:bg-right md:items-start bg-cover border-2 border-gray-200 shadow rounded-xl">
     
+    <!-- Heading -->
+    <h1 class="text-4xl font-extrabold max-w-[384px]  font-display text-gray-1400">Find your next<br>
+        <!-- Interactive Genres -->
+        {#key genre}
+            <span>
+                <h1 class="text-wrap max-w-[384px]  {genre.color}" transition:slide={{ easing: easing, duration: 250 }}>{genre.label}</h1>
+            </span>
+        {/key}
+
+        on folios.cc</h1>
+
+    <!-- Subheading -->
+    <p class="text-xl max-w-[384px] text-gray-800 text-pretty">Find & explore community builders for your Discord server.<br><br>Request features & get updates in our server below</p>
+    
+    <!-- CTA -->
+    <Button showIcon={true} hug={false} iconLeft="" iconRight={Discord} label="Join our Server" goTo="https://discord.gg/2ftBjsTePC"/>
 </div>
