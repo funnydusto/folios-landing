52497472406647ca04588a694876acf804b9958a
funnydusto <dustopersonal@gmail.com>
2024-07-06 06:29:05 -0400

    new ui, old ui is BROKEN

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import Navbar from "$components/Navbar.svelte";
 </script>
 
-<div class="flex flex-col gap-2 p-2 mx-auto bg-gray-300 h-dvh">
+
+<div class="flex flex-col gap-2 p-2 mx-auto h-dvh max-w-7xl">
     <slot />
 </div>
