51750cf294858eaad63358b4b92a93e9eabdd417
funnydusto <dustopersonal@gmail.com>
2024-07-06 06:20:11 -0400

    added marketing navbar

--- src/components/MarketingNavbar.svelte
+++ src/components/MarketingNavbar.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import Icon from "./Icon.svelte";
-    import FoliosLogoPrimary from "$lib/icons/folios-logo-primary.svg?raw";
+    import FoliosLogo from "$lib/icons/folios-logo-primary.svg?raw";
 </script>
 
-<div>
-    <Icon data={FoliosLogoPrimary}/>
-</div>
+<nav class="flex w-full px-6 py-4 -mt-2 bg-gray-200 rounded-b-xl shadow">
+    <!-- Left -->
+    <div class="flex gap-1.5 text-gray-800 fill-gray-800">
+        <Icon data={FoliosLogo}/>
+        <p class="font-extrabold font-display">folios.cc</p>
+    </div>
+</nav>
