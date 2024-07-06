575dd9c935866db3e0dcce32501066f408277ddf
funnydusto <dustopersonal@gmail.com>
2024-07-06 06:20:03 -0400

    added button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     export let label: string;
-    export let icon: string;
+    export let showIcon: boolean;
+    export let iconLeft: string;
+    export let iconRight: string;
     export let goTo: string;
+    export let hug: boolean;
 
     import Icon from "./Icon.svelte";
+    import CirclePlaceholder from "$lib/icons/circle-placeholder-dashed.svg?raw";
 </script>
 
-<button class="">
-    <span id="leftIcon"><Icon data={icon} /></span>
+<a href={goTo} class="flex bg-blue-400 text-gray-100 px-3 py-2 gap-2 md:max-w-[384px] rounded-xl justify-center hover:bg-blue-500 focus:ring-inset focus:ring-2 focus:ring-blue-500 transition-all disabled:opacity-50 {hug === true ? "w-fit" : "w-full"}">
+    <span class="{showIcon ? "visible" : "hidden"}" id="leftIcon"><Icon data={iconLeft} /></span>
     {label}
-    <Icon data={icon} />
-</button>
+    <span class="{showIcon ? "visible" : "hidden"}" id="rightIcon"><Icon data={iconRight} /></span>
+</a>
